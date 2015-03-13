913cdbe17dc44316a56ba9a2645c874194b5e19b
pfarnach <tefl.pat@gmail.com>
2015-03-13 13:39:04 -0700

    Switched over to ordinal x-axis using quantitative measurements

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var time_select = document.getElementById('time'),
 	height = 200,
 	width = 300,
 	padding = 50;
+	width_fraction = width / 4
 
 var skillz = d3.select('#skillz')
 				.append('svg')
@@ -12,20 +13,20 @@ var skillz = d3.select('#skillz')
 				.attr('id', 'viz')
 				.attr('transform', 'translate(' + padding * 2+ ',' + padding + ')');
 
-d3.csv('data.csv', function(data) {
+d3.csv('data-qual.csv', function(data) {
 
 	console.log(data);
 
 	// set scales
 	var yScale = d3.scale.ordinal()
-					.rangeRoundBands([0, 150], .2)
+					.rangeRoundBands([0, 150], .2)  // second parameter is for padding 
 					.domain(data.map(function(element) {
 						return element.skill;
 					}));
 
-	var xScale = d3.scale.linear()
-					.range([0, width])
-					.domain([0, 5]);
+	var xScale = d3.scale.ordinal()
+					.range([0, width_fraction * 1, width_fraction * 2, width_fraction * 3, width_fraction * 4])
+					.domain(["none", "newbie", "novice", "not bad", "good"]);
 
 	// set axes
 	var xAxis = d3.svg.axis().scale(xScale)
@@ -34,7 +35,7 @@ d3.csv('data.csv', function(data) {
 
 	var yAxis = d3.svg.axis().scale(yScale)
 								.orient('left')
-								.ticks(8);
+								.ticks(7);
 
 	// bind data to rectangles
 	var bars = skillz.selectAll('rect')
@@ -69,15 +70,15 @@ d3.csv('data.csv', function(data) {
 			})
 			.style('stroke', 'none')
 			.style('fill', function(d){
-				if (d[time] === "0") {
+				if (d[time] === "none") {
 					return "#D1FAFF";
-				} else if (d[time] === "1") {
+				} else if (d[time] === "newbie") {
 					return "#A8E0FF";
-				} else if (d[time] === "2") {
+				} else if (d[time] === "novice") {
 					return "#7CC6FE";
-				} else if (d[time] === "3") {
+				} else if (d[time] === "not bad") {
 					return "#0267C1";
-				} else if (d[time] === "4") {
+				} else if (d[time] === "good") {
 					return "#034078";
 				}
 			});
